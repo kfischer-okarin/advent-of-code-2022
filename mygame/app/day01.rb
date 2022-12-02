b094260511d2f57137ac8d306ce2d357bb5dcd47
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-12-02 22:26:32 +0900

    Render tilemap for day 01

--- mygame/app/day01.rb
+++ mygame/app/day01.rb
@@ -1,5 +1,12 @@
 class Day01
   def tick(args)
+    render(args)
+  end
+
+  def render(args)
+    args.outputs.primitives << {
+      x: 0, y: 0, w: 1280, h: 720, path: 'maps/day01/png/Level_0.png'
+    }.sprite!
   end
 end
 
