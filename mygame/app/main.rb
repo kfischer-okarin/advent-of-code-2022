06223251381515c524ec95ca3dc8bebbe89aa8ff
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-12-02 15:20:41 +0900

    Allow setup of scene

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -15,19 +15,25 @@ class SceneManager
   attr_accessor :next_scene
 
   def initialize(initial_scene)
-    @scenes = [initial_scene]
+    @scenes = []
+    @next_scene = initial_scene
   end
 
   def active_scene
     @scenes.last
   end
 
+  def pop_scene
+    @scenes.pop
+  end
+
   def tick(args)
-    active_scene.tick(args)
+    active_scene&.tick(args)
     return unless next_scene
 
     @scenes << next_scene
     @next_scene = nil
+    active_scene.setup(args) if active_scene.methods(false).include? :setup
   end
 end
 
